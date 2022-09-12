2efa45f470467bfa3f9fe689be86ab291c851110
cnearing <katerina4227@gmail.com>
2022-09-12 07:30:49 +0300

    add localization (58)

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,9 @@ module Bbq
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don"t generate system test files.
+
+    config.i18n.default_locale = :ru
+
     config.generators.system_tests = nil
   end
 end
