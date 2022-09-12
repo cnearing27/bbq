1d9bf1a7aeee6a734fc5bda5e2f9f9c38fea8c08
cnearing <katerina4227@gmail.com>
2022-09-12 08:12:42 +0300

    Revert "quotes to double quotes"
    This reverts commit 11c4be38bf2fe012d628c55fea3808493de9d2f6.

--- db/migrate/20220911230107_add_devise_to_users.rb
+++ db/migrate/20220911230107_add_devise_to_users.rb
@@ -42,7 +42,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
   end
 
   def self.down
-    # By default, we don"t want to make any assumption about how to roll back a migration when your
+    # By default, we don't want to make any assumption about how to roll back a migration when your
     # model already existed. Please edit below which fields you would like to remove in this migration.
     raise ActiveRecord::IrreversibleMigration
   end
