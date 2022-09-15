7180ef8d9d53869edd1de73d6cc6ee66bb418219
cnearing <katerina4227@gmail.com>
2022-09-15 22:49:38 +0300

    add validations to subscription model (event owner, email registered)

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -10,6 +10,9 @@ class Subscription < ApplicationRecord
 
   validates :user_name, presence: true, unless: -> { user.present? }
 
+  validate :email_registered, unless: -> { user.present? }
+  validate :is_owner, if: -> { user.present? }
+
   def user_name
     if user.present?
      user.name
@@ -23,6 +26,20 @@ class Subscription < ApplicationRecord
       user.email
     else
       super
+    end
+  end
+
+  private
+
+  def is_owner
+    if user == event.user
+      errors.add(:user, I18n.t("controllers.subscriptions.errors.owner"))
+    end
+  end
+
+  def email_registered
+    if User.find_by(email: user_email).present?
+      errors.add(:user_email, I18n.t("controllers.subscriptions.errors.email_registered"))
     end
   end
 end
