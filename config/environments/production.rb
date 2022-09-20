b5c487f5650185a7a17abcbaca51ba29f2eef1ec
cnearing <katerina4227@gmail.com>
2022-09-20 13:12:16 +0300

    production mode edited

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "bbq_production"
 
+  config.action_mailer.delivery_method = :mailjet
   config.action_mailer.default_url_options = {host: "bbq2-cnearing.herokuapp.com"}
   config.action_mailer.perform_caching = false
 
@@ -69,8 +70,6 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
-
-  config.action_mailer.delivery_method = :smtp
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
