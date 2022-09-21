58ae475a467f03756c944fee40cfb3476869bc1c
cnearing <katerina4227@gmail.com>
2022-09-21 23:08:36 +0300

    fix map.js rights

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,29 +1,31 @@
 function init(){
-  address = document.getElementById("map").getAttribute("data-address");
+  if (document.getElementById("map")) {
+    address = document.getElementById("map").getAttribute("data-address");
 
-  var myMap = new ymaps.Map("map", {
-      center: [55.76, 37.64],
-      zoom: 10
-  });
+    var myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 10
+    });
 
-  myGeocoder = ymaps.geocode(address);
+    myGeocoder = ymaps.geocode(address);
 
-  myGeocoder.then(
-    function (res) {
-      coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+    myGeocoder.then(
+      function (res) {
+        coordinates = res.geoObjects.get(0).geometry.getCoordinates();
 
-      myMap.geoObjects.add(
-          new ymaps.Placemark(
-            coordinates,
-            {iconContent: address},
-            {preset: "islands#blueStretchyIcon"}
-          )
-      );
+        myMap.geoObjects.add(
+            new ymaps.Placemark(
+              coordinates,
+              {iconContent: address},
+              {preset: "islands#blueStretchyIcon"}
+            )
+        );
 
-      myMap.setCenter(coordinates);
-      myMap.setZoom(15);
-    }, function (err) {
-      alert("Адрес не найден");
-    }
-  );
+        myMap.setCenter(coordinates);
+        myMap.setZoom(15);
+      }, function (err) {
+        alert("Адрес не найден");
+      }
+    );
+  }
 }
