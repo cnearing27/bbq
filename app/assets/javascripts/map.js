f5f0956d3ad657e9f655f1928787a1e863cbae40
cnearing <katerina4227@gmail.com>
2022-09-21 23:00:42 +0300

    fix console error ymaps on event page

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,10 +1,7 @@
-ymaps.ready(init);
-var myMap;
-
 function init(){
   address = document.getElementById("map").getAttribute("data-address");
 
-  myMap = new ymaps.Map("map", {
+  var myMap = new ymaps.Map("map", {
       center: [55.76, 37.64],
       zoom: 10
   });
