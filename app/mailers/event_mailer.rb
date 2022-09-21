85b8caec33cbab2c003d0370df62d64962d08b90
cnearing <katerina4227@gmail.com>
2022-09-21 21:53:22 +0300

    edit mailing text version

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,25 +1,22 @@
 class EventMailer < ApplicationMailer
-  def subscription(event, subscription, app_link)
+  def subscription(event, subscription)
     @email = subscription.user_email
     @name = subscription.user_name
     @event = event
-    @app_link = app_link
 
     mail to: event.user.email, subject: "Новая подписка на #{event.title}"
   end
 
-  def comment(event, comment, email, app_link)
+  def comment(event, comment, email)
     @comment = comment
     @event = event
-    @app_link = app_link
 
     mail to: email, subject: "Новый комментарий @ #{event.title}"
   end
 
-  def photo(event, photo, email, app_link)
+  def photo(event, photo, email)
     @photo = photo
     @event = event
-    @app_link = app_link
 
     mail to: email, subject: "Новая фотография @ #{event.title}"
   end
